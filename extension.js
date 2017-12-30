cbfd09b157fe22794289275c2e25d8c828dd3469
mechatroner <mechatroner@yandex.ru>
2017-12-29 20:47:52 -0500

    column name on hover

--- extension.js
+++ extension.js
@@ -3,27 +3,47 @@ const vscode = require('vscode');
 rainbow_utils = require('rainbow_utils')
 
 
-function guess_document_header(document, delim) {
+function guess_document_header(document, split_func) {
     var sampled_records = [];
     var num_lines = document.lineCount;
     var head_count = 10;
     if (num_lines <= head_count * 2) {
         for (var i = 1; i < num_lines; i++) {
-            sampled_records.push(rainbow_utils.split_simple_str(document.lineAt(i).text, delim, 0)[0]);
+            sampled_records.push(split_func(document.lineAt(i).text));
         }
     } else {
         for (var i = 1; i < head_count; i++) {
-            sampled_records.push(rainbow_utils.split_simple_str(document.lineAt(i).text, delim, 0)[0]);
+            sampled_records.push(split_func(document.lineAt(i).text));
         }
         for (var i = num_lines - head_count; i < num_lines; i++) {
-            sampled_records.push(rainbow_utils.split_simple_str(document.lineAt(i).text, delim, 0)[0]);
+            sampled_records.push(split_func(document.lineAt(i).text));
         }
     }
     if (sampled_records.length < 10)
         return null;
-    var potential_header = rainbow_utils.split_simple_str(document.lineAt(0).text, delim, 0)[0];
+    var potential_header = split_func(document.lineAt(0).text);
     var has_header = rainbow_utils.guess_if_header(potential_header, sampled_records);
     return has_header ? potential_header : null;
+}
+
+
+function make_hover_text(document, position, split_func, delim) {
+    var lnum = position.line;
+    var cnum = position.character;
+    var line = document.lineAt(lnum).text;
+    var report = split_func(line, delim, cnum);
+    var col_num = report[2];
+    var entries = report[0];
+    var result = 'col# ' + (col_num + 1);
+    split_func_simple = function(text) {
+        return split_func(text, delim, 0)[0];
+    }
+    var header = guess_document_header(document, split_func_simple);
+    if (header !== null && header.length == entries.length) {
+        var column_name = header[col_num];
+        result += ', "' + column_name + '"';
+    }
+    return result;
 }
 
 
@@ -37,23 +57,15 @@ function activate(context) {
 
     csv_provider = vscode.languages.registerHoverProvider('csv', {
         provideHover(document, position, token) {
-            var lnum = position.line;
-            var cnum = position.character;
-            var line = document.lineAt(lnum).text;
-            var fields = rainbow_utils.split_quoted_str(line, ',', cnum);
-            var col_num = fields[2];
-            return new vscode.Hover('col# ' + (col_num + 1));
+            hover_text = make_hover_text(document, position, rainbow_utils.split_quoted_str, ',');
+            return new vscode.Hover(hover_text);
         }
     });
 
     tsv_provider = vscode.languages.registerHoverProvider('tsv', {
         provideHover(document, position, token) {
-            var lnum = position.line;
-            var cnum = position.character;
-            var line = document.lineAt(lnum).text;
-            var fields = rainbow_utils.split_simple_str(line, '\t', cnum);
-            var col_num = fields[2];
-            return new vscode.Hover('col# ' + (col_num + 1));
+            hover_text = make_hover_text(document, position, rainbow_utils.split_simple_str, '\t');
+            return new vscode.Hover(hover_text);
         }
     });
 
