6c35758f51860b17c576f538ea70197a9e81570c
mechatroner <mechatroner@yandex.ru>
2020-10-25 15:28:35 -0400

    tests

--- test/extension.test.js
+++ test/extension.test.js
@@ -11,10 +11,18 @@ const fs = require('fs');
 const vscode = require('vscode');
 const rainbow_csv = require('../extension');
 
+const crypto = require('crypto');
+
 const log_file_path = path.join(__dirname, 'rainbow_csv.test.log');
+
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+function calc_str_md5(str) {
+    return crypto.createHash('md5').update(str, 'utf-8').digest('hex');
 }
 
 
@@ -39,10 +47,29 @@ suite("Extension Tests", function() {
         try {
             log_message('Starting tests');
             assert.equal(-1, [1, 2, 3].indexOf(0));
+
             const uri = vscode.Uri.file(path.join(__dirname, 'csv_files', 'university_ranking.csv'));
-            const document = await vscode.workspace.openTextDocument(uri);
-            const editor = await vscode.window.showTextDocument(document);
-            await sleep(3000);
+            const active_doc = await vscode.workspace.openTextDocument(uri);
+            const editor = await vscode.window.showTextDocument(active_doc);
+            let data = active_doc.getText();
+            let length_original = data.length;
+            log_message(`Original length: ${length_original}`)
+            await sleep(2000);
+
+            await vscode.commands.executeCommand('rainbow-csv.Align');
+            data = active_doc.getText();
+            let length_aligned = data.length;
+            log_message(`Aligned length: ${length_aligned}`)
+            await sleep(2000);
+
+            await vscode.commands.executeCommand('rainbow-csv.Shrink');
+            data = active_doc.getText();
+            let length_shrinked = data.length;
+            log_message(`Shrinked length: ${length_shrinked}`)
+            assert.equal(length_original, length_shrinked);
+
+
+            await sleep(1000);
             log_message('Finishing tests');
         } catch (e) {
             log_message('Error: tests have failed. Exception:');
