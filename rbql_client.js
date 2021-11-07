989a224366eb167113e2e737f1d2e404173cf5e3
mechatroner <mechatroner@yandex.ru>
2021-11-07 15:28:07 -0500

    improve ui

--- rbql_client.js
+++ rbql_client.js
@@ -14,6 +14,8 @@ var adjust_join_table_header_callback = null;
 
 var global_header = null;
 
+// FIXME add the tooltip messages to checkbox labels too.
+
 function report_backend_language_change() {
     let backend_language = document.getElementById('select_backend_language').value;
     vscode.postMessage({'msg_type': 'global_param_change', 'key': 'rbql_backend_language', 'value': backend_language});
