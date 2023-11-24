6cd8cc68325496f7598706410715f1f9a81c9e41
mechatroner <dmitry.tovich@gmail.com>
2023-11-23 20:04:13 -0500

    fix npm electron tests

--- test/runTest.js
+++ test/runTest.js
@@ -9,7 +9,7 @@ async function main() {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
         const extensionDevelopmentPath = path.resolve(__dirname, '../');
-        const testWorkspace = extensionDevelopmentPath;
+        const testWorkspace = path.resolve(__dirname);
         // The path to the extension test runner script
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, './suite/index');
